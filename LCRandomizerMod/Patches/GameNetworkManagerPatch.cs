089b81213f0833ebf4788c051c93f29b4df3346a
Tibnan <stibnan@gmail.com>
2024-05-25 21:23:03 +0200

    Fixed dictionary saving, finalized custom data saving

--- LCRandomizerMod/Patches/GameNetworkManagerPatch.cs
+++ LCRandomizerMod/Patches/GameNetworkManagerPatch.cs
@@ -10,7 +10,47 @@ namespace LCRandomizerMod.Patches
     {
         [HarmonyPatch(nameof(GameNetworkManager.Disconnect))]
         [HarmonyPostfix]
-        public static void SaveCustomValues(GameNetworkManager __instance)
+        public static void ClearDicts()
+        {
+            RandomizerModBase.mls.LogError("Clearing dicts");
+            RandomizerValues.ClearDicts(true);
+        }
+        //[HarmonyPatch(nameof(GameNetworkManager.Disconnect))]
+        //[HarmonyPrefix]
+        //public static void SaveCustomValues(GameNetworkManager __instance)
+        //{
+        //    if (Unity.Netcode.NetworkManager.Singleton.IsServer && StartOfRound.Instance.inShipPhase && __instance != null && !RandomizerValues.isDisconnected)
+        //    {
+        //        try
+        //        {
+        //            var runnableTypes = Assembly.GetExecutingAssembly().GetTypes().Where(t => typeof(ICustomValue).IsAssignableFrom(t) && !t.IsInterface);
+
+        //            foreach (var type in runnableTypes)
+        //            {
+        //                var instance = (ICustomValue)Activator.CreateInstance(type);
+        //                instance.SaveOnExit();
+        //            }
+        //            ES3.Save("keysToLoad", RandomizerValues.keysToLoad, GameNetworkManager.Instance.currentSaveFileName);
+
+        //            RandomizerModBase.mls.LogInfo("Saved dicts.");
+        //        } catch (Exception ex)
+        //        {
+        //            RandomizerModBase.mls.LogError("Exception caught during custom value serialization. " + ex.Message);
+        //            return;
+        //        }
+
+        //        RandomizerValues.ClearDicts(true);
+        //    }
+        //    else
+        //    {
+        //        RandomizerModBase.mls.LogError("Clearing client dicts");
+        //        RandomizerValues.ClearDicts(true);
+        //    }
+        //}
+
+        [HarmonyPatch(nameof(GameNetworkManager.SaveGame))]
+        [HarmonyPostfix]
+        public static void AutoSaveCustomItemData(GameNetworkManager __instance)
         {
             if (Unity.Netcode.NetworkManager.Singleton.IsServer && StartOfRound.Instance.inShipPhase && __instance != null)
             {
@@ -26,14 +66,17 @@ namespace LCRandomizerMod.Patches
                     ES3.Save("keysToLoad", RandomizerValues.keysToLoad, GameNetworkManager.Instance.currentSaveFileName);
 
                     RandomizerModBase.mls.LogInfo("Saved dicts.");
-                } catch (Exception ex)
+                }
+                catch (Exception ex)
                 {
                     RandomizerModBase.mls.LogError("Exception caught during custom value serialization. " + ex.Message);
                     return;
                 }
-
-                RandomizerValues.ClearDicts();
             }
-        }
+            else
+            {
+                return;
+            }
+        } 
     }
 }
